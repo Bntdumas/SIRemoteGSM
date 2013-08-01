79875e57ab7070891f28069774783328df87ea0e
Benoit Dumas <benoit.dumas@kdab.com>
2013-08-01 18:20:39 +0200

    Add commands to power up / reset the GSM module

--- mainApp/SIPunchToSMS.cpp
+++ mainApp/SIPunchToSMS.cpp
@@ -1,7 +1,37 @@
 #include "SIPunchToSMS.h"
 
+#include "wiringPi.h"
+
+#include <QTimer>
+
 SIPunchToSMS::SIPunchToSMS(const QString &GSMPort, const QString &SIPort, QObject *parent) :
     QObject(parent)
 {
+
+    int ret = wiringPiSetup();
+    if(ret == -1)
+        qFatal("failed to setup wiringPi lib.");
+    pinMode(Power, OUTPUT) ;
+    pinMode(Reset, OUTPUT)  ;
+
+    m_ButtonTimer = new QTimer(this);
+    m_ButtonTimer->setSingleShot(true);
+    connect(m_ButtonTimer, SIGNAL(timeout()), this, SLOT(releaseButton()));
 }
 
+void SIPunchToSMS::resetGSModule()
+{
+    digitalWrite(Reset, true) ;
+}
+
+void SIPunchToSMS::powerGSModule()
+{
+    digitalWrite(Power, true) ;
+    m_ButtonTimer->start(5000);
+}
+
+void SIPunchToSMS::releaseButtons()
+{
+    digitalWrite(Power, false) ;
+    digitalWrite(Reset, false) ;
+}
--- mainApp/SIPunchToSMS.h
+++ mainApp/SIPunchToSMS.h
@@ -7,17 +7,45 @@
  * @brief Connects to a Sport ident master unit (serial) and a GSM module (serial too) to emit SMS when somebody punch on the SI unit.
  */
 
+class QTimer;
+
 class SIPunchToSMS : public QObject
 {
     Q_OBJECT
 
 public:
     explicit SIPunchToSMS(const QString &GSMPort = QLatin1String("/dev/ttyAMA0"), const QString &SIPort = QLatin1String("/dev/ttyUSB0"), QObject *parent = 0);
-    
+
+    /**
+     * @brief Send a signal on the RST pin of the GSM module
+     */
+    void resetGSModule();
+
+    /**
+     * @brief Powers ON/OFF the module
+     */
+    void powerGSModule();
+
+    /**
+     * @brief Mapping between buttons
+     */
+    enum GSMModuleButtons {
+        Power = 0,
+        Reset = 0
+    };
+
 Q_SIGNALS:
     
 public Q_SLOTS:
-    
+
+private Q_SLOTS:
+    /**
+     * @brief press/release "physical" buttons on the GSM module
+     */
+    void releaseButtons();
+
+private:
+    QTimer *m_ButtonTimer;
 };
 
 #endif // SIPUNCHTOSMS_H
