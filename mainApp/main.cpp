af0cbc576a3f780c6b9366bbfa641c02c88eb571
Benoit Dumas <benoit.dumas@kdab.com>
2013-08-01 18:20:57 +0200

    instanciate main app

--- mainApp/main.cpp
+++ mainApp/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include "SIPunchToSMS.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    
+    SIPunchToSMS SiSms;
+    SiSms.powerGSModule();
     return a.exec();
 }
