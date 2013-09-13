3becdcbaea07a24e79a1cabf79180b384082c1d9
jan.dalheimer <jan@dalheimer.de>
2013-09-13 12:58:03 +0200

    Simplified Util::timeDifference

--- RemoteControlUI/util.cpp
+++ RemoteControlUI/util.cpp
@@ -1,7 +1,5 @@
 #include "util.h"
 
-#include <qmath.h>
-
 Util* Util::instance = new Util();
 
 Util::Util(QObject *parent) :
@@ -17,19 +15,8 @@ int Util::timeToSeconds(const QTime &time)
 
 QDateTime Util::timeDifference(const QTime &t1, const QTime &t2)
 {
-    // TODO optimize
-    int mseconds1 = t1.hour() * 60 * 60 * 1000 + t1.minute() * 60 * 1000 + t1.second() * 1000 + t1.msec();
-    int mseconds2 = t2.hour() * 60 * 60 * 1000 + t2.minute() * 60 * 1000 + t2.second() * 1000 + t2.msec();
-    int difference = mseconds1 - mseconds2;
-    if (difference < 0) {
-        return QDateTime(QDate::currentDate(), QTime());
-    }
-    int hours = qFloor(difference / 1000 / 60 / 60);
-    difference -= hours * 60 * 60 * 1000;
-    int minutes = qFloor(difference / 1000 / 60);
-    difference -= minutes * 60 * 1000;
-    int seconds = qFloor(difference / 1000);
-    difference -= seconds * 1000;
-    int mseconds = difference;
-    return QDateTime(QDate::currentDate(), QTime(hours, minutes, seconds, mseconds));
+    QTime time = QTime(0, 0, 0, 0);
+    time = time.addSecs(t2.secsTo(t1));
+    time = time.addMSecs(t1.msec() - t2.msec());
+    return QDateTime(QDate::currentDate(), time);
 }
